89088d0e6acba0b75b47129631d9ceeaa8389b5f
MO mursi <jemkarry@outlook.com>
2018-06-18 03:02:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+      array.join((" and "))
+  elsif array.length == 3
+      ar=array.join((", "))
+      ar.gsub(/(.*),(.*)/, '\1, and\2')
+  else 
+    ar=array.join((", "))
+    ar.gsub(/(.*),(.*)/, '\1, and\2')
+  end
 end
